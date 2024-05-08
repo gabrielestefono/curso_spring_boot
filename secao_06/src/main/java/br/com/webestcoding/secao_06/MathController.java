ec9bc75dae91f478d9fe0b9ed49f6bc6eb8d0ab3
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-08 17:37:08 -0300

    Implementar outras ações

--- secao_06/src/main/java/br/com/webestcoding/secao_06/MathController.java
+++ secao_06/src/main/java/br/com/webestcoding/secao_06/MathController.java
@@ -6,14 +6,50 @@ import br.com.webestcoding.exceptions.UnsupportedMathOperationException;
 
 @RestController
 public class MathController {
+    private final String MENSAGEM = "Por favor, use um valor numérico!";
 
-    @GetMapping(value = "/sum/{numberOne}/{numberTwo}")
-    public double sum(@PathVariable("numberOne") String numberOne,
+    @GetMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo) throws Exception {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new UnsupportedMathOperationException("Por favor, use um valor numérico!");
+            throw new UnsupportedMathOperationException(this.MENSAGEM);
         }
         return (convertToDouble(numberOne) + convertToDouble(numberTwo));
+    }
+
+    @GetMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(@PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException(this.MENSAGEM);
+        }
+        return (convertToDouble(numberOne) - convertToDouble(numberTwo));
+    }
+
+    @GetMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(@PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException(this.MENSAGEM);
+        }
+        return (convertToDouble(numberOne) / convertToDouble(numberTwo));
+    }
+
+    @GetMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(@PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException(this.MENSAGEM);
+        }
+        return (convertToDouble(numberOne) * convertToDouble(numberTwo));
+    }
+
+    @GetMapping("/sqrt/{numberOne}")
+    public Double sqrt(@PathVariable("numberOne") String numberOne) throws Exception {
+        if (!isNumeric(numberOne)) {
+            throw new UnsupportedMathOperationException(this.MENSAGEM);
+        }
+        return (Math.sqrt(convertToDouble(numberOne)));
     }
 
     private Double convertToDouble(String strNumber) {
