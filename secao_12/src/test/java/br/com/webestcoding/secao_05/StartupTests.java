00b5de557704d835e1d644dab8711153230dd599
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-14 09:53:00 -0300

    Finalizando Seção 12

---
+++ secao_12/src/test/java/br/com/webestcoding/secao_05/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.webestcoding.secao_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
