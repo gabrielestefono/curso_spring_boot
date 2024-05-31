14c16e59239fdb482a6f61bf9da01c6afa3e9d23
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-31 16:19:29 -0300

    Injetando propriedades customizadas

--- Chad Derby/Secao_01/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ Chad Derby/Secao_01/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,21 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class FunRestController {
+	// Properties from application.properties
+
+	@Value("${coach.name}")
+	private String coachName;
+
+	@Value("${team.name}")
+	private String teamName;
+
+
 	/**
 	 * Expose a "/" that returns a "Hello World!"
 	 * @return String
@@ -31,5 +41,10 @@ public class FunRestController {
 	@GetMapping("/fortune")
 	public String getDailyFortune() {
 		return "Tokay is your lucky day!";
+	}
+
+	@GetMapping("/teaminfo")
+	public String teamInfo() {
+		return "Coach name: " + coachName + " Team name: " + teamName;
 	}
 }
