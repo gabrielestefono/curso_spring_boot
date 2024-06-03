00a455855279cf8c9c740443bfc31bc8a092b39d
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-06-03 19:10:47 -0300

    Injeção de dependências

--- Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CrickedCode.java
+++ Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CrickedCode.java
@@ -7,6 +7,6 @@ public class CrickedCode implements Coach{
 
 	@Override
 	public String getDailyWorkOut() {
-		return "Practice fast bowling for 15 minutes!";
+		return "Practice fast bowling for 20 minutes!";
 	}
 }
--- Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,17 +1,18 @@
 package com.luv2code.springcoredemo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springcoredemo.common.Coach;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 public class DemoController {
-	private final Coach myCoach;
+	private Coach myCoach;
 
-	public DemoController(Coach myCoach) {
+	@Autowired
+	public void setMyCoach(Coach myCoach) {
 		this.myCoach = myCoach;
 	}
 
