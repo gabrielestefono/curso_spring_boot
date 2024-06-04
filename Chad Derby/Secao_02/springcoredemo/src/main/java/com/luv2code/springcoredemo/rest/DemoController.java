1709d0551ffaa04574c0aed1ad392df2bfb3ff7e
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-06-04 18:41:27 -0300

    Using Primary

--- Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,6 +1,5 @@
 package com.luv2code.springcoredemo.rest;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ import com.luv2code.springcoredemo.common.Coach;
 public class DemoController {
 	private final Coach myCoach;
 
-	public DemoController(@Qualifier("tennisCoach") Coach myCoach) {
+	public DemoController(Coach myCoach) {
 		this.myCoach = myCoach;
 	}
 
