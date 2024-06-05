c40df6116944805ead6d77c54b8e77356565bea9
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-06-05 05:53:45 -0300

    Bean Scopes

--- Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -7,16 +7,30 @@ import org.springframework.web.bind.annotation.RestController;
 import com.luv2code.springcoredemo.common.Coach;
 
 
+
 @RestController
 public class DemoController {
 	private final Coach myCoach;
 
-	public DemoController(@Qualifier("cricketCoach") Coach myCoach) {
+	private final Coach anotherCoach;
+
+	public DemoController(
+			@Qualifier("cricketCoach") Coach myCoach,
+			@Qualifier("cricketCoach") Coach anotherCoach
+		) {
+		System.out.println("In constructor: " + getClass().getSimpleName());
 		this.myCoach = myCoach;
+		this.anotherCoach = anotherCoach;
 	}
 
 	@GetMapping("/dailyworkout")
 	public String getMethodName() {
 		return this.myCoach.getDailyWorkOut();
 	}
+
+	@GetMapping("/check")
+	public String check() {
+		return "Comáring Beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
+	}
+	
 }
