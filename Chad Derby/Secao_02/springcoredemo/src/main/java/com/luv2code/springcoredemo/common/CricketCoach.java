4ea2c997cccd4b306d5d7e8d85b28165bf0ddcb3
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-06-05 18:28:34 -0300

    Java Cofig Beans

--- Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ Chad Derby/Secao_02/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -2,25 +2,10 @@ package com.luv2code.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-
 @Component
 public class CricketCoach implements Coach{
 	public CricketCoach(){
 		System.out.println("In constructor " + getClass().getSimpleName());
-	}
-
-	// Define our init method
-	@PostConstruct
-	public void doMyStartupStuff(){
-		System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
-	}
-
-	// Define our destroy method
-	@PreDestroy
-	public void doMyCleanStuff(){
-		System.out.println("In doMyCleanStuff(): " + getClass().getSimpleName());
 	}
 
 	@Override
